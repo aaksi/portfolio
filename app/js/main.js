8210b5b4b0f930df290457e9d16d49e1b58e1ccd
aaksi <yyff91@mail.ru>
2021-03-08 12:36:52 +0300

    finish

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,33 @@
 $(function(){
-  
 
+    $(window).scroll(function() {
+        var sticky = $('.header'),
+          scroll = $(window).scrollTop();
+         
+        if (scroll >= 80) { 
+          sticky.addClass('fixed'); }
+        else { 
+         sticky.removeClass('fixed');
+        }
+      });
+
+      $('.menu__list-link').on( 'click', function(){ 
+        var el = $(this);
+        var dest = el.attr('href'); 
+        if(dest !== undefined && dest !== '') { 
+            $('html').animate({ 
+                scrollTop: $(dest).offset().top 
+            }, 500 
+            );
+        }
+        return false;
+    });
+      
+    $('.menu__btn').on('click', function(){
+        $(this).toggleClass('menu__btn--active');
+        $('.menu__list').toggleClass('menu__list--active');
+    });
+
+  
+      
 });
